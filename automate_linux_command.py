e39438403c9359b7d9aa07fc08dfeda384659043
Omprakashkr1 <112559646+Omprakashkr1@users.noreply.github.com>
2025-06-11 17:46:48 +0530

    first commit

---
+++ automate_linux_command.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+import subprocess
+gemini_api_key = "AIzaSyB9fHK2qUPekNQRd4jVZDUbxYWmK-xukqc"
+model = OpenAI(api_key = gemini_api_key , base_url = "https://generativelanguage.googleapis.com/v1beta/openai/")
+def my_model(prompt):
+        mymsg = [
+            {"role" : "system" , "content": "you have to act as a devops engineer and give commands to the user whatever they want to perform . just give the linux commands to the user no need of any other stuffs. the content should be like if the user is asking for the date right now then you should simple give them output as date ."},
+            {"role" : "user" , "content" : prompt}
+            ]
+        output = model.chat.completions.create(
+            messages = mymsg,
+            model="gemini-2.0-flash"
+        )
+        return output.choices[0].message.content
